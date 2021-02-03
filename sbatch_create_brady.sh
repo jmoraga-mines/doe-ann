09148c57b3178dd6777ad74eeb537f6ba0c6a1c4
Jim Moraga <jmoraga@mines.edu>
2021-02-02 21:02:07 -0700

    Updated sbatch files to run the ANNs, and logs from data creation

--- sbatch_create_brady.sh
+++ sbatch_create_brady.sh
@@ -1,5 +1,5 @@
 #!/bin/env bash
-#SBATCH --job-name=ann_createdata       # Job name
+#SBATCH --job-name=ann_createdata_b       # Job name
 #SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=tmoraga@gmail.com # Where to send mail      
 #SBATCH --ntasks=1                    # Run on a single CPU
@@ -7,9 +7,10 @@
 # Don't use #SBATCH -w, --nodelist=g005,c026,c028
 #SBATCH --mem=32G                     # Job memory request
 #SBATCH --time=72:00:00               # Time limit hrs:min:sec
-#SBATCH --output=ann_createdata_%j.log   # Standard output and error log
-#SBATCH -p gpu                       # run only in "gpu" nodes, we need tensorflow
-#SBATCH --exclusive                  # run only this job
+#SBATCH --output=ann_createdata_b_%j.log   # Standard output and error log
+##SBATCH -p gpu       # run only in "gpu" nodes, we need aensorflow
+#SBATCH -p compute    # run only in "compute" nodes, we don't need tensorflow
+#SBATCH --exclusive   # run only this job
 echo `pwd`
 echo `hostname`; date
 echo "Current shell:"
@@ -32,10 +33,27 @@ then
    rm -rf brady_samples_19x3d
 fi
 echo "Running time(python...brady -k 19 -s 1000)"
-time(python create_doe_dataset.py -i ../doe-data/brady_som_output.gri -c 5 -d brady_samples_19x3a -k 19 -s 1000)
+time(python create_doe_dataset.py -i ../doe-data/brady_som_output.gri -c 3 -d brady_samples_19x3a -k 19 -s 1000)
 echo "Running time(python...brady -k 19 -s 100000)"
-time(python create_doe_dataset.py -i ../doe-data/brady_som_output.gri -c 5 -d brady_samples_19x3d -k 19 -s 100000)
+time(python create_doe_dataset.py -i ../doe-data/brady_som_output.gri -c 3 -d brady_samples_19x3d -k 19 -s 100000)
 echo "files under: brady_samples_19x3d/0"
 find brady_samples_19x3d/0 -type f | wc -l
 echo "files under: brady_samples_19x3d/1"
 find brady_samples_19x3d/1 -type f | wc -l
+
+
+## Now create the images wth 5 layers
+if [ -d "brady_samples_19x5a" ]
+then
+   echo "Samples directory exists, deleting"
+   rm -rf brady_samples_19x5a
+   rm -rf brady_samples_19x5d
+fi
+echo "Running time(python...brady -k 19 -s 1000)"
+time(python create_doe_dataset.py -i ../doe-data/brady_som_output.gri -c 3 -d brady_samples_19x5a -k 19 -s 1000)
+echo "Running time(python...brady -k 19 -s 100000)"
+time(python create_doe_dataset.py -i ../doe-data/brady_som_output.gri -c 3 -d brady_samples_19x5d -k 19 -s 100000)
+echo "files under: brady_samples_19x5d/0"
+find brady_samples_19x5d/0 -type f | wc -l
+echo "files under: brady_samples_19x5d/1"
+find brady_samples_19x5d/1 -type f | wc -l
